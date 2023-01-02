84656097003d6b3721fb28a35fb7ec617744ccb5
Anas <55194408+Anas-Ehab@users.noreply.github.com>
2023-01-02 14:39:42 +0800

    Sprint 3 Final Upload
    The final version of the code after fixing the error.

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -11,15 +11,12 @@ import '../controllers/cart_controller.dart';
 Future<void> init() async {
   //api client
   Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
-//https://mvs.bslmeiyu.com
-  //repos//https://www.dbestech.com
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
   Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
   Get.lazyPut(() => CartRepo());
 
   //controllers
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
-  Get.lazyPut(
-      () => RecommendedProductController(recommendedProductRepo: Get.find()));
+  Get.lazyPut(() => RecommendedProductController(recommendedProductRepo: Get.find()));
   Get.lazyPut(() => CartController(cartRepo: Get.find()));
 }
